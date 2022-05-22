9ff40485d846643433a2e4e00f81e003775de226
yashcybrot <79310401+yashcybrot@users.noreply.github.com>
2022-05-22 12:39:21 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -22,7 +22,7 @@ export default class Command extends BaseCommand {
         filteredMap.forEach(
             (user, index) =>
                 (text += `#${index + 1}\n🎫 *Username: ${
-                    user.notify || user.vname || user.name || 'null'
+                    user.notify || user.vname || user.name || 'HACKTIVIST'
                 }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
         text += `\nTo deploy your own Bot or To support Kaoi👾\nVisit : https://github.com/PrajjwalDatir/Kaoi `
